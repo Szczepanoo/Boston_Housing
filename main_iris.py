29d17ed1fe291636059a2f194f42d836db965860
Jakub <jakub.szczepanski02@gmail.com>
2024-04-23 14:12:12 +0200

    made some changes

--- main_iris.py
+++ main_iris.py
@@ -2,12 +2,25 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def sigmoid_activation(X):
-    return 1/(1+np.exp(-X))
-def sigmoid_derivative(X):
-    return X*(1-X)
+
+# def sigmoid_activation(X):
+#     return 1/(1+np.exp(-X))
+#
+#
+# def sigmoid_derivative(X):
+#     return X*(1-X)
+
+
+def step_function(X):
+    X[X>0] = 1
+    X[X<=0] = 0
+    return X
+
+
 def predict(X, W):
-    return sigmoid_activation(np.dot(X, W))
+    return np.dot(X,W)
+
+
 
 data = pd.read_csv('iris.csv')
 
@@ -18,7 +31,7 @@ y = data.iloc[:100, -1]
 y = np.where(y == 'Iris-setosa', 1, 0)
 bias_col = np.ones((X.shape[0],1))
 
-X = np.hstack((X,bias_col))
+X = np.hstack((X,np.ones((X.shape[0],1))))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
@@ -29,13 +42,21 @@ W = np.random.randn(X.shape[1])
 
 losses = []
 for epoch in range(epochs):
-    predictions = predict(X_train, W)
-    error = predictions - y_train
-    loss = np.sum(error**2)
+    preds = step_function(predict(X_train, W))
+    errors = preds - y_train
+    loss = np.sum(errors **2)
     losses.append(loss)
 
-    d = error * sigmoid_derivative(predictions)
-    gradient = np.dot(X_train.T , d)
+    W += -alpha * np.dot(X_train.T, errors)
 
-    W += -alpha * gradient
+    # for example, label in zip(X_train, y_train):
+    #     p = step_function(predict(example, W))
+    #     if p != label:
+    #         error = p - label
+    #         loss = error ** 2
+    #         losses.append(loss)
+    #         W += -alpha * error * example
+
+p = step_function(predict(X_test,W))
+print('error sum:', np.sum(p-y_test))
 print(losses)
