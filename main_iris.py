97a3b4216b00158e6a135080a8e4dd8d585fbbb3
Jakub <jakub.szczepanski02@gmail.com>
2024-04-23 14:42:25 +0200

    implemented perceptron as class

--- main_iris.py
+++ main_iris.py
@@ -3,60 +3,43 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-# def sigmoid_activation(X):
-#     return 1/(1+np.exp(-X))
-#
-#
-# def sigmoid_derivative(X):
-#     return X*(1-X)
+class Perceptron:
+    def __init__(self,N, epochs = 10, alpha = 0.1):
+        self.epochs = epochs
+        self.alpha = alpha
+        self.W = np.random.randn(N)
 
+    def activation(self, X):
+        X = np.where(X>=0,1,0)
+        return X
+    def fit(self, X, y):
+        losses = []
+        for epoch in range(self.epochs):
+            p = self.predict(X)
+            errors = p - y
+            loss = np.sum(errors ** 2) / 2
+            losses.append(loss)
+            self.W += -self.alpha * np.dot(X.T, errors)
 
-def step_function(X):
-    X[X>0] = 1
-    X[X<=0] = 0
-    return X
+        print(f'losses: {losses}')
 
+    def predict(self, X):
+        return self.activation(np.dot(X,self.W))
 
-def predict(X, W):
-    return np.dot(X,W)
 
 
 
 data = pd.read_csv('iris.csv')
 
-X = data.drop('Id', axis = 1)
-
-X = X.iloc[:100, :-1]
+X = data.iloc[:100, 1:-1]
 y = data.iloc[:100, -1]
 y = np.where(y == 'Iris-setosa', 1, 0)
-bias_col = np.ones((X.shape[0],1))
 
-X = np.hstack((X,np.ones((X.shape[0],1))))
+X= np.hstack((X, np.ones((X.shape[0], 1))))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
+P = Perceptron(X.shape[1],epochs=20)
+P.fit(X_train, y_train)
 
-epochs = 50
-alpha = 0.01
-
-W = np.random.randn(X.shape[1])
-
-losses = []
-for epoch in range(epochs):
-    preds = step_function(predict(X_train, W))
-    errors = preds - y_train
-    loss = np.sum(errors **2)
-    losses.append(loss)
-
-    W += -alpha * np.dot(X_train.T, errors)
-
-    # for example, label in zip(X_train, y_train):
-    #     p = step_function(predict(example, W))
-    #     if p != label:
-    #         error = p - label
-    #         loss = error ** 2
-    #         losses.append(loss)
-    #         W += -alpha * error * example
-
-p = step_function(predict(X_test,W))
-print('error sum:', np.sum(p-y_test))
-print(losses)
+pred = y_test - P.predict(X_test)
+print(np.sum(y_test - P.predict(X_test)))
